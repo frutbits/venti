436f9bc0037189815255749d60fd415bdadaafa2
ZenShibata <zenshibata@outlook.com>
2022-04-10 21:14:21 +0700

    feat(preconditions): use CommandContext

--- src/preconditions/isNodeAvailable.ts
+++ src/preconditions/isNodeAvailable.ts
@@ -6,6 +6,14 @@ import { Precondition, PreconditionOptions, PreconditionResult } from "@sapphire
 })
 export class isNodeAvailable extends Precondition {
     public chatInputRun(): PreconditionResult {
+        return this.precondition();
+    }
+
+    public messageRun(): PreconditionResult {
+        return this.precondition();
+    }
+
+    private precondition(): PreconditionResult {
         return [...this.container.client.shoukaku.nodes.values()].some(n => !n.destroyed) ? this.ok() : this.error({ message: "There's no node available" });
     }
 }
--- src/preconditions/memberInSameVoice.ts
+++ src/preconditions/memberInSameVoice.ts
@@ -1,20 +1,33 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Precondition, PreconditionOptions, PreconditionResult } from "@sapphire/framework";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, Message } from "discord.js";
+import { CommandContext } from "../structures/CommandContext";
 
 @ApplyOptions<PreconditionOptions>({
     name: "memberInSameVoice"
 })
 export class memberInSameVoice extends Precondition {
     public chatInputRun(interaction: CommandInteraction<"cached">): PreconditionResult {
-        const dispatcher = this.container.client.shoukaku.queue.get(interaction.guildId);
-        const voiceChannel = interaction.member.voice.channel;
+        return this.precondition(new CommandContext(interaction));
+    }
+
+    public messageRun(message: Message): PreconditionResult {
+        return this.precondition(new CommandContext(message));
+    }
+
+    private precondition(ctx: CommandContext): PreconditionResult {
+        const dispatcher = this.container.client.shoukaku.queue.get(ctx.context.guildId!);
+        const voiceChannel = ctx.context.member!.voice.channel;
         if (dispatcher) {
             if (dispatcher.listeners.length && voiceChannel?.joinable) {
                 dispatcher.destroy();
                 return this.ok();
             }
-            if (interaction.guild.me?.voice.channelId && interaction.guild.me.voice.channelId !== voiceChannel?.id && dispatcher.listeners.length > 0 && !voiceChannel?.joinable) {
+            if (
+                ctx.context.guild!.me?.voice.channelId &&
+                ctx.context.guild!.me.voice.channelId !== voiceChannel?.id &&
+                dispatcher.listeners.length > 0 && !voiceChannel?.joinable
+            ) {
                 return this.error({ message: `I'm already being used in ${voiceChannel!.toString()}` });
             }
         }
