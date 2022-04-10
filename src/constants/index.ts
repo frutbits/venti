815b0305995cce0e02792a8e525bf99f6e866fad
ZenShibata <zenshibata@outlook.com>
2022-04-10 17:50:23 +0700

    feat(constants): change environment key value

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,10 @@
-export const embedInfoColor = process.env.EMBED_INFO_COLOR!;
+export const embedInfoColor = process.env.CONFIG_EMBED_INFO_COLOR!;
 export const Emojis = {
-    YES: process.env.EMOJI_YES!,
-    NO: process.env.EMOJI_NO!
+    YES: process.env.CONFIG_EMOJI_YES!,
+    NO: process.env.CONFIG_EMOJI_NO!
 };
 
-if (typeof process.env.EMBED_INFO_COLOR !== "string") throw new Error("EMBED_INFO_COLOR must be a hex color string");
+if (typeof process.env.CONFIG_EMBED_INFO_COLOR !== "string") throw new Error("CONFIG_EMBED_INFO_COLOR must be a hex color string");
 for (const emoji of Object.entries(Emojis)) {
-    if (typeof emoji[1] !== "string") throw new Error(`EMOJI_${emoji[0]} must be a string`);
+    if (typeof emoji[1] !== "string") throw new Error(`CONFIG_${emoji[0]} must be a string`);
 }
