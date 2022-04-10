1721053ef62fd6ad48bc538552df650da10e54fc
ZenShibata <zenshibata@outlook.com>
2022-04-10 17:42:10 +0700

    feat(Venti): initialize Util class

--- src/structures/Venti.ts
+++ src/structures/Venti.ts
@@ -5,9 +5,11 @@ import { SapphireClient } from "@sapphire/framework";
 import { ClientOptions } from "discord.js";
 import { ShoukakuHandler } from "./ShoukakuHandler";
 import * as constants from "../constants";
+import { Util } from "../utils/Util";
 
 export class Venti extends SapphireClient {
     public readonly shoukaku = new ShoukakuHandler(this);
+    public readonly util = new Util(this);
     public readonly constants = constants;
     public constructor(opt: ClientOptions) {
         super(opt);
