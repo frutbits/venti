95eeca58e0a137a41868f0a1c0c2e66a204aea03
ZenShibata <zenshibata@outlook.com>
2022-04-15 18:43:16 +0700

    feat(listeners): assign message id

--- src/listeners/shoukaku/playerTrackEnd.ts
+++ src/listeners/shoukaku/playerTrackEnd.ts
@@ -1,6 +1,8 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Listener } from "@sapphire/framework";
-import { ShoukakuPlayer, TrackStartEvent } from "shoukaku";
+import { ShoukakuPlayer, TrackEndEvent } from "shoukaku";
+import { LoopType } from "../../structures/Dispatcher";
+import { Util } from "../../utils/Util";
 
 @ApplyOptions<Listener.Options>({
     emitter: "shoukaku",
@@ -12,9 +14,27 @@ export class ShoukakuPlayerTrackEndListener extends Listener {
         super(context, options);
     }
 
-    public run(player: ShoukakuPlayer, data: TrackStartEvent): any {
+    public async run(player: ShoukakuPlayer, data: TrackEndEvent): Promise<any> {
         const dispatcher = this.container.client.shoukaku.queue.get(data.guildId);
         if (!dispatcher) return undefined;
-        // TODO: Handle queue shifting here
+        if (data.reason === "REPLACED") return;
+        dispatcher.queue.previousTrack = dispatcher.queue[0];
+        dispatcher.queue.shift();
+        if (["LOAD_FAILED", "CLEAN_UP"].includes(data.reason)) {
+            if (dispatcher.queue.length) return player.playTrack(dispatcher.queue[0].base64);
+        }
+        if (dispatcher.loopState === LoopType.ALL) dispatcher.queue.push(dispatcher.queue.previousTrack);
+        if (dispatcher.loopState === LoopType.ONE) dispatcher.queue.unshift(dispatcher.queue.previousTrack);
+        void dispatcher.embedPlayer?.update();
+        if (dispatcher.queue.length) return player.playTrack(dispatcher.queue[0].base64);
+        if (!dispatcher.embedPlayer?.message) {
+            dispatcher.oldMusicMessage = await dispatcher.textChannel.send({
+                embeds: [
+                    Util.createEmbed("info", "We've run out of songs! Better queue up some more tunes.")
+                        .setAuthor({ name: "Queue Concluded" })
+                ]
+            }).then(x => x.id);
+        }
+        return dispatcher.destroy();
     }
 }
--- src/listeners/shoukaku/playerTrackStart.ts
+++ src/listeners/shoukaku/playerTrackStart.ts
@@ -1,6 +1,7 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Listener } from "@sapphire/framework";
 import { ShoukakuPlayer, TrackStartEvent } from "shoukaku";
+import { Util } from "../../utils/Util";
 
 @ApplyOptions<Listener.Options>({
     emitter: "shoukaku",
@@ -15,8 +16,13 @@ export class ShoukakuPlayerTrackStartListener extends Listener {
     public async run(player: ShoukakuPlayer, data: TrackStartEvent): Promise<any> {
         const dispatcher = this.container.client.shoukaku.queue.get(data.guildId);
         if (!dispatcher) return;
-        await dispatcher.textChannel.send({
-            content: `Started playing: \`${dispatcher.queue[0].info.title!}\``
-        });
+        void dispatcher.embedPlayer?.update();
+        if (!dispatcher.embedPlayer?.message) {
+            dispatcher.oldMusicMessage = await dispatcher.textChannel.send({
+                embeds: [
+                    Util.createEmbed("info", `Started playing: \`${dispatcher.queue[0].info.title!}\``)
+                ]
+            }).then(x => x.id);
+        }
     }
 }
