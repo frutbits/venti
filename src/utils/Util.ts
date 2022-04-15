37086739246557873b45f5689c7b1630e8d3aaab
ZenShibata <zenshibata@outlook.com>
2022-04-15 18:40:13 +0700

    feat(Util): add readableTime function

--- src/utils/Util.ts
+++ src/utils/Util.ts
@@ -23,6 +23,31 @@ export class Util {
         return embed;
     }
 
+    public static readableTime(duration: number): string {
+        const SECOND = 1000;
+        const MINUTE = SECOND * 60;
+        const HOUR = MINUTE * 60;
+        const seconds = Math.floor(duration / SECOND) % 60;
+        if (duration < MINUTE) return `00:${seconds.toString().padStart(2, "0")}`;
+        const minutes = Math.floor(duration / MINUTE) % 60;
+        let output = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+        if (duration >= HOUR) {
+            const hours = Math.floor(duration / HOUR);
+            output = `${hours.toString().padStart(2, "0")}:${output}`;
+        }
+        return output;
+    }
+
+    public static chunk<T>(arr: T[], len: number): T[][];
+    public static chunk(arr: string, len: number): string[];
+    public static chunk(...args: any[]): any[] {
+        const [arr, len] = args as [any, number];
+        const rest: (typeof arr)[] = [];
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+        for (let i = 0; i < arr.length; i += len) { rest.push(arr.slice(i, i + len)); }
+        return rest;
+    }
+
     public static isValidURL(url: string): boolean {
         try {
             new URL(url);
