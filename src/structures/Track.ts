0516659de5a8edb9323142be899abe50dd274cef
ZenShibata <zenshibata@outlook.com>
2022-04-15 18:40:41 +0700

    feat(Track): add more getter

--- src/structures/Track.ts
+++ src/structures/Track.ts
@@ -1,7 +1,13 @@
 import { ShoukakuTrack } from "shoukaku";
 
-export class Track extends ShoukakuTrack {
-    public constructor(raw: object, public readonly requester: string) {
-        super(raw);
+export class Track {
+    public thumbnail!: string;
+    public base64 = this.track.track;
+    public info = this.track.info;
+    public constructor(public readonly track: ShoukakuTrack, public readonly requester: string) {}
+
+    public get displayThumbnail(): string {
+        if (this.thumbnail) return this.thumbnail;
+        return `https://i3.ytimg.com/vi/${this.info.identifier!}/maxresdefault.jpg`;
     }
 }
