fa46a071fc275646438f9d9f4955b6798bd85a4b
ZenShibata <zenshibata@outlook.com>
2022-04-16 12:49:52 +0700

    feat(config): add NODE_ENV

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import { NodeOptions } from "shoukaku";
 
 export const devs: string[] = JSON.parse(process.env.CONFIG_DEVS ?? "[]");
 export const devGuilds = JSON.parse(process.env.CONFIG_DEV_GUILDS ?? "[]");
+export const isDev = process.env.NODE_ENV === "DEVELOPMENT";
 export const prefix = process.env.CONFIG_PREFIX!;
 export const lavalink = {
     servers: JSON.parse(process.env.LAVALINK_NODES ?? "[]") as NodeOptions[],
@@ -16,3 +17,4 @@ export const lavalink = {
 };
 
 if (typeof process.env.CONFIG_PREFIX !== "string") throw new Error("CONFIG_PREFIX must be a string");
+if (typeof process.env.NODE_ENV !== "string" || !["DEVELOPMENT", "PRODUCTION"].includes(process.env.NODE_ENV)) throw new Error("NODE_ENV must be a either DEVELOPMENT or PRODUCTION");
