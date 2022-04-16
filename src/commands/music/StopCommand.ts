edcff28d319488cca21ba839af2102bb254eb9b9
ZenShibata <zenshibata@outlook.com>
2022-04-16 15:24:23 +0700

    feat: add stop command

---
+++ src/commands/music/StopCommand.ts
@@ -0,0 +1,48 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { ApplicationCommandRegistry, Args, Command, RegisterBehavior } from "@sapphire/framework";
+import { CommandInteraction, Message } from "discord.js";
+import { devGuilds } from "../../config";
+import { CommandContext } from "../../structures/CommandContext";
+import { Util } from "../../utils/Util";
+
+@ApplyOptions<Command.Options>({
+    aliases: [],
+    name: "stop",
+    description: "Stop and delete current queue",
+    preconditions: ["isPlayerExists", "memberInVoice", "memberVoiceJoinable", "memberInSameVoice"],
+    chatInputCommand: {
+        register: true
+    },
+    requiredClientPermissions: ["EMBED_LINKS"]
+})
+export class StopCommand extends Command {
+    public override registerApplicationCommands(registry: ApplicationCommandRegistry): void {
+        registry.registerChatInputCommand({
+            name: this.name,
+            description: this.description
+        }, {
+            registerCommandIfMissing: true,
+            guildIds: devGuilds,
+            behaviorWhenNotIdentical: RegisterBehavior.Overwrite
+        });
+    }
+
+    public async chatInputRun(interaction: CommandInteraction<"cached">): Promise<any> {
+        return this.run(new CommandContext(interaction));
+    }
+
+    public messageRun(message: Message, args: Args): Promise<any> {
+        return this.run(new CommandContext(message, args));
+    }
+
+    public async run(ctx: CommandContext): Promise<any> {
+        const dispatcher = this.container.client.shoukaku.queue.get(ctx.context.guildId!);
+        if (dispatcher?.embedPlayer?.textChannel?.id === ctx.context.channelId) ctx.isInsideRequesterChannel = true;
+        dispatcher?.destroy();
+        return ctx.send({
+            embeds: [
+                Util.createEmbed("success", "Stopped and deleted current queue.", true)
+            ]
+        });
+    }
+}
