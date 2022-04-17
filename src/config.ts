7972a12004ec52d07ed873edcadd59340cab6043
ZenShibata <zenshibata@outlook.com>
2022-04-17 10:40:41 +0700

    chore(config): add deleteQueueTimeout

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ export const devs: string[] = JSON.parse(process.env.CONFIG_DEVS ?? "[]");
 export const devGuilds = JSON.parse(process.env.CONFIG_DEV_GUILDS ?? "[]");
 export const isDev = process.env.NODE_ENV === "DEVELOPMENT";
 export const prefix = process.env.CONFIG_PREFIX!;
+export const deleteQueueTimeout = Number(process.env.CONFIG_DELETE_QUEUE_TIMEOUT!) * 1000;
 export const lavalinkRest: { host?: string; auth?: string } = JSON.parse(process.env.LAVALINK_REST ?? "{}");
 export const lavalink = {
     servers: JSON.parse(process.env.LAVALINK_NODES ?? "[]") as NodeOptions[],
@@ -18,6 +19,7 @@ export const lavalink = {
 };
 
 if (typeof process.env.CONFIG_PREFIX !== "string") throw new Error("CONFIG_PREFIX must be a string");
+if (typeof process.env.CONFIG_DELETE_QUEUE_TIMEOUT !== "string" || isNaN(deleteQueueTimeout) || deleteQueueTimeout < 1) throw new Error("CONFIG_DELETE_QUEUE_TIMEOUT must be a number");
 if (typeof process.env.NODE_ENV !== "string" || !["DEVELOPMENT", "PRODUCTION"].includes(process.env.NODE_ENV)) throw new Error("NODE_ENV must be a either DEVELOPMENT or PRODUCTION");
 if (typeof process.env.LAVALINK_NODES !== "string") throw new Error("LAVALINK_NODES must be an array string (\"[]\")");
 if (typeof process.env.LAVALINK_REST !== "string") throw new Error("LAVALINK_REST must be an object string (\"{}\")");
